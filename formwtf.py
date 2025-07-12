2af202e068ae9fa7e9b94fb80932fc73b8c812af
MiansaibX7 <mianmwaqas77@gmail.com>
2025-07-12 12:05:59 +0500

    Initial commit

---
+++ formwtf.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class RegistrationForm(FlaskForm):
+    
+    name = StringField("Full Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+class NoteForm(FlaskForm):
+
+    content = StringField("Note", validators=[DataRequired()])
+    submit = SubmitField("Save Note")
+
